e434c225e02474e0fe50238459f680b67212c141
Beniamin Savu <benisavu@gmail.com>
2016-07-14 15:31:26 +0300

    Implement Retrieve Images Service

--- webapp/src/main/java/org/iqu/webapp/others/RetrieveImageService.java
+++ webapp/src/main/java/org/iqu/webapp/others/RetrieveImageService.java
@@ -2,14 +2,25 @@ package org.iqu.webapp.others;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/images")
+/**
+ * 
+ * @author Beniamin Savu
+ * 
+ *         Service that retrieves images, based on their ID as returned by the
+ *         retrieve news/event endpoint. It should return the full binary
+ *         object.
+ *
+ */
+@Path("/images/{imageId}")
 public class RetrieveImageService {
 
 	@GET
-	public Response retrieveImage(){
+	public Response retrieveImage(@PathParam("imageId") String imageId) {
+
 		return Response.ok().build();
 	}
-	
+
 }
